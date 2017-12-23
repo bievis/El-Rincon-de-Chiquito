73fb3df7b50315819a3ae1879c02f6fe79cae379
Diego Moreno Montes <e2diego@gmail.com>
2017-12-23 12:23:25 +0100

    Initial Commit

---
+++ El Rincon de Chiquito/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  El Rincon de Chiquito
+//
+//  Created by Diego on 23/12/17.
+//  Copyright © 2017 Quijote@pps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
